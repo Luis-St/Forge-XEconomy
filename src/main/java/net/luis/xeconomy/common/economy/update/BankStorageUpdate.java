89a64df7dd59a37214d2d1c4bc32c77e03398d5f
BuildTools <76595940+Luis-st@users.noreply.github.com>
2022-01-11 21:20:14 +0100

    remove update stuff

--- src/main/java/net/luis/xeconomy/common/economy/update/BankStorageUpdate.java
+++
@@ -1,7 +0,0 @@
-package net.luis.xeconomy.common.economy.update;
-
-public record BankStorageUpdate(int money, int id, int repaidMoney) {
-	
-	public static final BankStorageUpdate NO_UPDATE = new BankStorageUpdate(0, -1, 0);
-	
-}
--- src/main/java/net/luis/xeconomy/common/economy/update/PlayerStorageUpdate.java
+++
@@ -1,7 +0,0 @@
-package net.luis.xeconomy.common.economy.update;
-
-public record PlayerStorageUpdate(int money, double inflation) {
-	
-	public static final PlayerStorageUpdate NO_UPDATE = new PlayerStorageUpdate(0, 0);
-	
-}
